d74cc43e3773fb4351d0f3032e08d983301cc8df
europe-for-everyone <57996166+europe-for-everyone@users.noreply.github.com>
2021-02-18 17:45:11 +0100

    Fixes

--- api/routes/routeProva.js
+++ api/routes/routeProva.js
@@ -4,22 +4,63 @@ const axios = require("axios");
 
 const API = 'https://dev-manager.opus.plus/api/OP';
 
+const checkAuth = (result, res) => {
+    if (result.data.data.errors) {
+        var error = result.data?.data?.errors[0]?.description;
+        if (error == "TOKEN NOT FOUND" || error == "Session expired") {
+            res.status(401).json({
+                error: "No authorization"
+            })
+        }
+    } else {
+        res.send(result.data);
+    }
+}
 
 
 router.get('/GetRepertories', (req, res, send) => {
-    const {token, allDocs, search, lang} = req.query
+    
+    const { token, allDocs, search, lang } = req.query
     axios.get(`${API}/GetRepertories?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
         .then((result) => {
-            if (result.data.data.errors) {
-                var error = result.data?.data?.errors[0]?.description;
-                if (error == "TOKEN NOT FOUND" || error == "Session expired") {
-                    res.status(401).json({
-                        error: "No authorization"
-                    })
-                }
-            } else {
-                res.send(result.data);
-            }
+            checkAuth(result, res);
+        })
+});
+
+router.get('/GetChapters', (req, res, send) => {
+    
+    const { token, allDocs, search, lang } = req.query
+    axios.get(`${API}/GetChapters?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
+        .then((result) => {
+            checkAuth(result, res);
+        })
+});
+
+router.get('/GetSymptoms', (req, res, send) => {
+    
+    const { token, allDocs, search, lang } = req.query
+    axios.get(`${API}/GetSymptoms?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
+        .then((result) => {
+            checkAuth(result, res);
+        })
+});
+
+router.get('/GetReferences', (req, res, send) => {
+    
+    const { token, allDocs, search, lang } = req.query
+    axios.get(`${API}/GetReferences?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
+        .then((result) => {
+            checkAuth(result, res);
+        })
+});
+
+
+router.get('/LoginUser', (req, res, send) => {
+    
+    const { token, allDocs, search, lang } = req.query
+    axios.get(`${API}/GetReferences?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
+        .then((result) => {
+            checkAuth(result, res);
         })
 });
 
--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-app.use("/prova", provaRoutes);
+app.use("/api", provaRoutes);
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
