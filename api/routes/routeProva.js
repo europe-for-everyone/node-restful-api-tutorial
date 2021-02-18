b152326ff81df40188bc44d7e36768c33dbab6be
europe-for-everyone <57996166+europe-for-everyone@users.noreply.github.com>
2021-02-18 17:23:01 +0100

    fix

--- api/routes/routeProva.js
+++ api/routes/routeProva.js
@@ -6,15 +6,19 @@ const API = 'https://dev-manager.opus.plus/api/OP';
 
 
 
-router.get('/', (req, res, send) => {
-    console.log("req", req);
-
-    axios.get(`${API}/GetRepertories?token=0195674add0d006c209dfd010889fcd10&search=synth&lang=it,en&allDocs=true`)
+router.get('/GetRepertories', (req, res, send) => {
+    const {token, allDocs, search, lang} = req.query
+    axios.get(`${API}/GetRepertories?token=${token}&allDocs=${allDocs}&search=${search}&lang=${lang}`)
         .then((result) => {
-            if (result.data?.data?.errors[0]?.description == "TOKEN NOT FOUND") {
-                res.status(401).json({
-                    error: "No authorization"
-                });
+            if (result.data.data.errors) {
+                var error = result.data?.data?.errors[0]?.description;
+                if (error == "TOKEN NOT FOUND" || error == "Session expired") {
+                    res.status(401).json({
+                        error: "No authorization"
+                    })
+                }
+            } else {
+                res.send(result.data);
             }
         })
 });
